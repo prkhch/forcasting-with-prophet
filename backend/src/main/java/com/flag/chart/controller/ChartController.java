b706d96174a6caf73237600d5335108754fc92fa
PARKHEECHANG <shjc4623@gmail.com>
2024-01-18 22:03:22 +0900

    feat: post MultipartFile

--- backend/src/main/java/com/flag/chart/controller/ChartController.java
+++ backend/src/main/java/com/flag/chart/controller/ChartController.java
@@ -2,6 +2,7 @@ package com.flag.chart.controller;
 
 import com.flag.chart.domain.Chart;
 import com.flag.chart.dto.CreateChartRequest;
+import com.flag.chart.dto.XlsFileResponse;
 import com.flag.chart.service.ChartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,9 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,4 +26,14 @@ public class ChartController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedChart);
     }
+
+    @PostMapping("/api/xls")
+    public ResponseEntity<XlsFileResponse> postXlsFile(@RequestParam("file") MultipartFile file) {
+        XlsFileResponse xlsFileResponse = new XlsFileResponse();
+        xlsFileResponse.setFileName(file.getOriginalFilename());
+        xlsFileResponse.setFileSize(file.getSize());
+        xlsFileResponse.setFileType(file.getContentType());
+        return ResponseEntity.ok(xlsFileResponse);
+    }
+
 }
