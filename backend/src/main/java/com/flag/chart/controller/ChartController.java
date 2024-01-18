667949ce91053857b2715a5d5cc6e06034eff532
PARKHEECHANG <shjc4623@gmail.com>
2024-01-19 07:30:00 +0900

    feat: react <-> spring boot <-> flask

--- backend/src/main/java/com/flag/chart/controller/ChartController.java
+++ backend/src/main/java/com/flag/chart/controller/ChartController.java
@@ -4,6 +4,7 @@ import com.flag.chart.domain.Chart;
 import com.flag.chart.dto.CreateChartRequest;
 import com.flag.chart.dto.XlsFileResponse;
 import com.flag.chart.service.ChartService;
+import com.flag.chart.service.FlaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +15,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RequiredArgsConstructor
 @RestController
 public class ChartController {
 
     private final ChartService chartService;
+    private final FlaskService flaskService;
 
     @PostMapping("/api/chart")
     public ResponseEntity<Chart> createChart(@RequestBody @Validated CreateChartRequest request) {
@@ -28,12 +32,17 @@ public class ChartController {
     }
 
     @PostMapping("/api/xls")
-    public ResponseEntity<XlsFileResponse> postXlsFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> postXlsFile(@RequestParam("file") MultipartFile file) throws IOException {
         XlsFileResponse xlsFileResponse = new XlsFileResponse();
         xlsFileResponse.setFileName(file.getOriginalFilename());
         xlsFileResponse.setFileSize(file.getSize());
         xlsFileResponse.setFileType(file.getContentType());
-        return ResponseEntity.ok(xlsFileResponse);
+        xlsFileResponse.setFileData(file.getBytes());;
+        System.out.println("xlsFile : " + xlsFileResponse);
+
+        ResponseEntity<String> flaskResponse = flaskService.sendFileToPandas(xlsFileResponse);
+
+        return flaskResponse;
     }
 
 }
