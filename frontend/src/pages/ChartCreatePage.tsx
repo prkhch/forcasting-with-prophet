93b365fdab08a161538df12d5a4e716387d30495
PARKHEECHANG <shjc4623@gmail.com>
2024-01-19 12:56:12 +0900

    feat: DataSet Update

--- frontend/src/pages/ChartCreatePage.tsx
+++ frontend/src/pages/ChartCreatePage.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useRef } from "react";
 import axios from "axios";
+import { DataItem } from "types/DataItem";
+import { spawn } from "child_process";
+import useForamatDate from "hooks/useForamatDate";
 
 const ChartCreatePage = () => {
   // 파일
@@ -20,17 +23,38 @@ const ChartCreatePage = () => {
     setMemberId(e.target.value);
   };
 
+  // 파일 업로드
+  const [dataSet, setDataSet] = useState<DataItem[]>([]);
+  const handleInputChange = (index: number, key: string, value: string) => {
+    const newData = [...dataSet];
+    const newItem = { ...newData[index], [key]: value };
+    newData[index] = newItem;
+    setDataSet(newData);
+  };
   const handleChangeUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (!files) return;
     formData.append("file", files[0]);
     console.log(formData.get("file"));
   };
-
   const handleClickUpload = () => {
     fileInput.current?.click();
   };
+  const ApiPostExcelFile = () => {
+    console.log(formData.get("file"));
+    axios
+      .post("/api/xls", formData)
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        setDataSet(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
+  // 등록
   const ApiPostCreateChart = () => {
     console.log(title, content, memberId);
     axios
@@ -50,30 +74,12 @@ const ChartCreatePage = () => {
       });
   };
 
-  const ApiPostExcelFile = () => {
-    console.log(formData.get("file"));
-    axios
-      .post("/api/xls", formData)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
   return (
     <div>
       <div>등록 페이지</div>
-      <input
-        type="file"
-        ref={fileInput}
-        onChange={handleChangeUpload}
-        style={{ display: "none" }}
-      />
-      <button onClick={handleClickUpload}>업로드</button>
-      <button onClick={ApiPostExcelFile}>엑셀전송</button>
-      <br />
+
+      <hr />
+
       <input type="text" onChange={handleChangeTitle} defaultValue="제목" />
       <input
         type="textarea"
@@ -85,6 +91,46 @@ const ChartCreatePage = () => {
         onChange={handleChangeMemberId}
         defaultValue="작성자"
       />
+
+      <hr />
+
+      <input
+        type="file"
+        ref={fileInput}
+        onChange={handleChangeUpload}
+        style={{ display: "none" }}
+      />
+      <button onClick={handleClickUpload}>업로드</button>
+      <button onClick={ApiPostExcelFile}>엑셀전송</button>
+
+      {dataSet.length > 0 && (
+        <div>
+          {Object.keys(dataSet[0]).map((key) => (
+            <input key={`header-${key}`} type="text" value={key} readOnly />
+          ))}
+        </div>
+      )}
+
+      {dataSet.map((item, index) => (
+        <div key={index}>
+          {Object.entries(item).map(([key, value]) => {
+            const isDate = key.includes("Date");
+            const inputValue = isDate ? useForamatDate(value) : value;
+
+            return (
+              <input
+                key={key}
+                type={isDate ? "date" : "text"}
+                value={inputValue}
+                onChange={(e) => handleInputChange(index, key, e.target.value)}
+              />
+            );
+          })}
+        </div>
+      ))}
+
+      <hr />
+
       <button onClick={ApiPostCreateChart}>차트전송</button>
     </div>
   );
