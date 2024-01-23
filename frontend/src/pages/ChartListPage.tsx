b426f79abee84b4164590a19e444e677ee451e96
PARKHEECHANG <shjc4623@gmail.com>
2024-01-24 08:02:12 +0900

    feat: get articleList

--- frontend/src/pages/ChartListPage.tsx
+++ frontend/src/pages/ChartListPage.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Article } from "types/Article";
 
 const ChartListPage = () => {
+  const navigate = useNavigate();
+  const [articleList, setArticleList] = useState<Article[]>([]);
+
+  // getLst
+  const ApiGetArtilceList = () => {
+    axios
+      .get("/api/articles", {})
+      .then((res) => {
+        console.log(res.data);
+        setArticleList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    ApiGetArtilceList();
+  }, []);
+
   return (
     <div>
       <div>리스트 페이지</div>
+
+      <div>
+        {articleList.map((article, idx) => (
+          <div key={idx} onClick={() => navigate(`article/${article.id}`)}>
+            <div>{article.id}</div>
+            <div>{article.title}</div>
+            <div>{article.content}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
