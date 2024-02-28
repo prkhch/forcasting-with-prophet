87b6964f2f67c631d6d4a660c17c23d0169d3f09
PARKHEECHANG <shjc4623@gmail.com>
2024-02-29 00:56:34 +0900

    refactor: main

--- frontend/src/styles/mainPage/StyledContainer.tsx
+++ frontend/src/styles/mainPage/StyledContainer.tsx
@@ -10,7 +10,7 @@ const Animation = keyframes`
 `;
 
 export const StyledContainer = styled.div`
-  background-image: linear-gradient(#ffffff, rgba(47, 112, 175, 0.5)), url("main/bg.jfif");
+  background-image: linear-gradient(rgba(255, 255, 255, 1), rgba(47, 112, 175, 0.3)), url("main/bg.jfif");
   background-size: cover;
   background-position: center;
   height: 100vh;
