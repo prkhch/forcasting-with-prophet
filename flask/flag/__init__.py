1633d5af568dc5d50d3c3c8000afed6c866d11ea
prkhch <shjc4623@gmail.com>
2025-03-12 00:36:09 +0900

    feat : flask-cors

--- flask/flag/__init__.py
+++ flask/flag/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     from .views import main_views
     app.register_blueprint(main_views.bp)
