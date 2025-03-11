ddf5acfdf01c50c44a34c35b871b7a4e8b6c8802
prkhch <shjc4623@gmail.com>
2025-03-11 23:31:57 +0900

    feat : spring-boot cors

--- backend/src/main/java/com/flag/flag/controller/FlagController.java
+++ backend/src/main/java/com/flag/flag/controller/FlagController.java
@@ -29,6 +29,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @RestController
 public class FlagController {
