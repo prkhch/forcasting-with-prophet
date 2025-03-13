fccda84aa9d75260c5f217da6f9f215212908fd7
prkhch <shjc4623@gmail.com>
2025-03-14 01:20:19 +0900

    chore : check pem key

--- flask/flag/__init__.py
+++ flask/flag/__init__.py
@@ -1,5 +1,8 @@
+import os
 from flask import Flask
 from flask_cors import CORS
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def create_app():
     app = Flask(__name__)
@@ -12,4 +15,10 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
+
+    cert_path = os.path.join(BASE_DIR, "cert.pem")
+    key_path = os.path.join(BASE_DIR, "key.pem")
+
+    print(f"Using SSL cert: {cert_path}, key: {key_path}")
+
     app.run(host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem'))
